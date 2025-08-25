646410a7b03891d4d1c9f799bd9f3ec8ec7c0ded
indrajit-intglobal <indrajit.ghosh@intglobal.com>
2025-08-25 14:54:49 +0530

    new push

--- App.js
+++ App.js
@@ -1,16 +1,20 @@
-import 'react-native-gesture-handler';
-import React from 'react';
-import { NavigationContainer } from '@react-navigation/native';
-import RootNavigator from './src/navigation/RootNavigator';
-import { Provider } from 'react-redux';
-import { store } from './src/store';
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, Text, View } from 'react-native';
 
 export default function App() {
   return (
-    <Provider store={store}>
-      <NavigationContainer>
-        <RootNavigator />
-      </NavigationContainer>
-    </Provider>
+    <View style={styles.container}>
+      <Text>Open up App.js to start working on your app!</Text>
+      <StatusBar style="auto" />
+    </View>
   );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
--- src/data/products.js
+++
@@ -1,5 +0,0 @@
-export const products = [
-  { id: 1, name: 'Dumbbell Set', price: 49.99, image: 'https://via.placeholder.com/150' },
-  { id: 2, name: 'Treadmill', price: 499.99, image: 'https://via.placeholder.com/150' },
-  { id: 3, name: 'Bench Press', price: 299.99, image: 'https://via.placeholder.com/150' },
-];
